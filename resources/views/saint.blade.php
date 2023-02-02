9b6ef894e76ee47233691d4f805e0c628f0a6875
Jessica <jesscara.993@gmail.com>
2023-02-02 17:16:59 +0100

    bonus 2: style to saint page

--- resources/views/saint.blade.php
+++ resources/views/saint.blade.php
@@ -1,8 +1,21 @@
 @extends('layout/main-layout')
 
 @section('content')
-    <div>{{$saint -> name}}</div>
-    <div>{{$saint -> place_of_birth}}</div>
-    <div>{{$saint -> given_blessing}}</div>
-    <div>{{$saint -> miracles_number}}</div>
+<div class="container text-center py-5 my_saint">
+  <div>
+    <span>Nome:</span> {{$saint -> name}}
+  </div>
+
+  <div>
+    <span>Luogo di nascita:</span> {{$saint -> place_of_birth}}
+  </div>
+
+  <div>
+    <span>Benedizione:</span> {{$saint -> given_blessing}}
+  </div>
+
+  <div>
+    <span>Miracoli:</span> {{$saint -> miracles_number}}
+  </div>
+</div>
 @endsection
