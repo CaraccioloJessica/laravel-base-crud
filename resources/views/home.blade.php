8457a6243dfca0adc68df3f09085f724ccc81d0f
Jessica <jesscara.993@gmail.com>
2023-02-02 16:23:58 +0100

    bonus 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layout.main-layout')
 
 @section('content')
-  <h1>Hello</h1>
+  <h1>Saints</h1>
+  <ul>
+    @foreach ($saints as $saint)
+    <li>
+      Nome Santo: {{$saint -> name}} <br>
+      N. Miracoli: {{$saint -> miracles_number}}
+    </li>       
+    @endforeach
+  </ul>
 @endsection
